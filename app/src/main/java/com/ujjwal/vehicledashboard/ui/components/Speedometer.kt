19891b4d9c227f2fd5ace17dfb92d38b8467310a
Ujjwal kumar <ujjwalthakur7474@gmail.com>
2026-07-04 15:45:36 +0530

    "Add speedometer tick marks"

--- app/src/main/java/com/ujjwal/vehicledashboard/ui/components/Speedometer.kt
+++ app/src/main/java/com/ujjwal/vehicledashboard/ui/components/Speedometer.kt
@@ -27,5 +27,27 @@ fun Speedometer(
             radius = 8f,
             center = Offset(size.width / 2, size.height / 2)
         )
+
+        repeat(37) { index ->
+
+            val angle = Math.toRadians((135 + index * 7.5).toDouble())
+
+            val start = Offset(
+                x = center.x + (size.minDimension / 2 - 40f) * kotlin.math.cos(angle).toFloat(),
+                y = center.y + (size.minDimension / 2 - 40f) * kotlin.math.sin(angle).toFloat()
+            )
+
+            val end = Offset(
+                x = center.x + (size.minDimension / 2 - 20f) * kotlin.math.cos(angle).toFloat(),
+                y = center.y + (size.minDimension / 2 - 20f) * kotlin.math.sin(angle).toFloat()
+            )
+
+            drawLine(
+                color = Color.DarkGray,
+                start = start,
+                end = end,
+                strokeWidth = 4f
+            )
+        }
     }
 }
